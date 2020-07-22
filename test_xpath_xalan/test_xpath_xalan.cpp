47a969061eebf1ee26349d3ec633ecc578aa8104
Chris8018 <tranuytrieuvi@protonmail.com>
2020-07-22 18:43:43 +0700

    link test file to xerces and xalan

--- test_xpath_xalan/test_xpath_xalan.cpp
+++ test_xpath_xalan/test_xpath_xalan.cpp
@@ -1,8 +1,14 @@
 ﻿#include "test_xpath_xalan.h"
 
+#include <xercesc/dom/DOM.hpp>
+
+#include <xalanc/XPath/Function.hpp>
+#include <xalanc/XPathCAPI/XPathCAPI.h>
+
 #include <iostream>
 
 int main()
 {
+	std::cout << "Hello" << std::endl;
 	return 0;
 }
